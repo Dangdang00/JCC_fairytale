14924ab52c85cd38d52d98374bda7df02a127912
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-07 09:48:38 +0900

    사과 피하기 게임 추가

---
+++ JCC_fairytale/Assets/Script/W_Script/AppleGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleGenerator : MonoBehaviour {
+    public GameObject DApple_white_Prefab;
+    float span = 1.0f;
+    float delta = 0;
+
+    void Update() {
+        this.delta += Time.deltaTime;
+        if(this.delta > this.span) {
+            this.delta = 0;
+            GameObject go = Instantiate(DApple_white_Prefab) as GameObject;
+            int px = Random.Range(-10, 10);
+            go.transform.position = new Vector3(px, 7, 0);
+        }
+    }
+}
---
+++ JCC_fairytale/Assets/Script/W_Script/WhiteGameWay.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WhiteGameWay : MonoBehaviour
+{
+    public void GameWay()
+    {
+        SceneManager.LoadScene("WhiteGameWay");
+    }
+}
