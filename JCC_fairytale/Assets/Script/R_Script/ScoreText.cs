05d197088fa2f320e3760a5ee997ec1c5620a065
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-19 20:23:59 +0900

    스코어 기능

--- JCC_fairytale/Assets/Script/R_Script/ScoreText.cs
+++ JCC_fairytale/Assets/Script/R_Script/ScoreText.cs
@@ -6,10 +6,21 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     int score = 0;
+    public Text scoreText;
 
-    public void ScoreUp(int number)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        score += number;
-        GetComponent<Text>().text = "Score : " + score;
+        if(col.gameObject.tag == "stone")
+        {
+            Debug.Log("Touch!");
+
+            // 돌멩이 삭제
+            Destroy(col.gameObject, 0f);
+
+            // 스코어 기능
+            score += 5;
+            scoreText.text = score.ToString();
+
+        }
     }
 }
