89606f225502eabc8ee647146bc0dd22a92d1bbf
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-19 20:48:02 +0900

    돌멩이 색깔별 다른 스코어

--- JCC_fairytale/Assets/Script/R_Script/ScoreText.cs
+++ JCC_fairytale/Assets/Script/R_Script/ScoreText.cs
@@ -6,21 +6,29 @@ using UnityEngine.UI;
 public class ScoreText : MonoBehaviour
 {
     int score = 0;
+    int stone = 5;
+    int rstone = 10;
+
     public Text scoreText;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "stone")
         {
-            Debug.Log("Touch!");
-
-            // 돌멩이 삭제
-            Destroy(col.gameObject, 0f);
-
-            // 스코어 기능
-            score += 5;
-            scoreText.text = score.ToString();
+            Debug.Log("stone_Touch!");
+            score += stone;
 
         }
+        else if(col.gameObject.tag == "rstone")
+        {
+            Debug.Log("rstone_Touch!");
+            score += rstone;
+        }
+
+        // 돌멩이 삭제
+        Destroy(col.gameObject, 0f);
+
+        // 스코어 출력
+        scoreText.text = score.ToString();
     }
 }
