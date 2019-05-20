9cf0aaacb957ca274c9e0a8c3b9bb5196d4b5433
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-20 10:13:17 +0900

    백설공주 미니게임 디자인 변경 및 기능 추가

---
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/Apple_time.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Apple_time : MonoBehaviour
+{
+    public static float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 30f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (time != 0)
+        {
+            time -= Time.deltaTime;
+            if (time <= 0)
+            {
+                time = 0;
+            }
+        }
+
+        int t = Mathf.FloorToInt(time);
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Time : " + t.ToString();
+    }
+}
