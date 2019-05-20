a03c57df7b62a2009311de4810e2c606f94ab824
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-20 18:30:20 +0900

    타이틀 화면, 메인스토리 연결

---
+++ JCC_fairytale/Assets/Script/Go_Story.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go_Story : MonoBehaviour
+{
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("story");
+    }
+}
