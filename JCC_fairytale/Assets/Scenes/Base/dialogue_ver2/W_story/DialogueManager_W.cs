65183ce756ed2190cdfdc984d48d04c1721cf54d
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2019-05-21 12:47:24 +0900

    백설공주 앞부분 스토리

---
+++ JCC_fairytale/Assets/Scenes/Base/dialogue_ver2/W_story/DialogueManager_W.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager_W : MonoBehaviour
+{
+
+    public static DialogueManager_afterR instance;
+
+    public Text text;
+    public Text name;
+    public SpriteRenderer rendererSprite_R;
+    public SpriteRenderer rendererSprite_L;
+    public SpriteRenderer rendererBackground;
+
+    private List<string> listSentences;
+    private List<string> listNames;
+    private List<Sprite> listSprites_R;
+    private List<Sprite> listSprites_L;
+    private List<Sprite> listBackground;
+
+    private int count; // 대화 진행 상황 카운트.
+
+    private bool talking = false;
+
+    #region Singleton
+    /* private void Awake()
+   {
+       if (instance == null)
+       {
+           DontDestroyOnLoad(this.gameObject);
+           instance = this;
+       }
+       else
+       {
+           Destroy(this.gameObject);
+       }
+   }*/
+    #endregion Singleton
+
+    // Use this for initialization
+    public void Start()
+    {
+        count = 0;
+        //text.text = " ";
+        //name.text = " ";
+        listSentences = new List<string>();
+        listNames = new List<string>();
+        listSprites_R = new List<Sprite>();
+        listSprites_L = new List<Sprite>();
+        listBackground = new List<Sprite>();
+    }
+
+    public void ShowDialogue(Dialogue_22 dialogue)
+    {
+        talking = true;
+        for (int i = 0; i < dialogue.sentences.Length; i++)
+        {
+            listBackground.Add(dialogue.background[i]);
+            listSprites_R.Add(dialogue.sprites_R[i]);
+            listSprites_L.Add(dialogue.sprites_L[i]);
+            listSentences.Add(dialogue.sentences[i]);
+            listNames.Add(dialogue.names[i]);
+        }
+        StartCoroutine(Start_DialogueCoroutine());
+    }
+
+    public void ExitDialogue()
+    {
+        text.text = " ";
+        name.text = " ";
+        count = 0;
+        listSentences.Clear();
+        listNames.Clear();
+        listSprites_R.Clear();
+        listSprites_L.Clear();
+        listBackground.Clear();
+        talking = false;
+    }
+
+
+    IEnumerator Start_DialogueCoroutine()
+    {
+        if (count > 0)
+        {
+            if (listBackground[count] != listBackground[count - 1])
+            {
+                //yield return new WaitForSeconds(0.2f);
+                rendererBackground.GetComponent<SpriteRenderer>().sprite = listBackground[count];
+                rendererSprite_R.GetComponent<SpriteRenderer>().sprite = listSprites_R[count];
+                rendererSprite_L.GetComponent<SpriteRenderer>().sprite = listSprites_L[count];
+            }
+            else
+            {
+                if (listSprites_R[count] != listSprites_R[count - 1])
+                {
+                    //yield return new WaitForSeconds(0.1f);
+                    rendererSprite_R.GetComponent<SpriteRenderer>().sprite = listSprites_R[count];
+                }
+
+                else if (listSprites_L[count] != listSprites_L[count - 1])
+                {
+                    //yield return new WaitForSeconds(0.1f);
+                    rendererSprite_L.GetComponent<SpriteRenderer>().sprite = listSprites_L[count];
+                }
+            }
+        }
+        else
+        {
+            yield return new WaitForSeconds(0.05f);
+            rendererBackground.GetComponent<SpriteRenderer>().sprite = listBackground[count];
+            rendererSprite_R.GetComponent<SpriteRenderer>().sprite = listSprites_R[count];
+            rendererSprite_L.GetComponent<SpriteRenderer>().sprite = listSprites_L[count];
+        }
+
+        name.text += listNames[count];
+        for (int i = 0; i < listSentences[count].Length; i++)
+        {
+            text.text += listSentences[count][i]; // 1글자씩 출력.
+            yield return new WaitForSeconds(0.01f);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (talking)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                count++;
+                text.text = " ";
+                name.text = " ";
+
+                if (count == listSentences.Count)
+                {
+                    StopAllCoroutines();
+                    ExitDialogue();
+                    SceneManager.LoadScene("WhiteSnowMain");
+                }
+                else
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(Start_DialogueCoroutine());
+                }
+            }
+        }
+    }
+}
---
+++ JCC_fairytale/Assets/Scenes/Base/dialogue_ver2/W_story/TestDialogue_W.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogue_W : MonoBehaviour
+{
+    [SerializeField]
+    public Dialogue_22 dialogue;
+    private DialogueManager_W theDM_3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDM_3 = FindObjectOfType<DialogueManager_W>();
+        theDM_3.ShowDialogue(dialogue);
+    }
+}
