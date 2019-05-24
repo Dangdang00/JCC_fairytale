c49802793a73d64298415a4c827e4cbac6746be9
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-24 16:32:41 +0900

    백설공주 황금 사과

---
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/gold.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gold : MonoBehaviour
+{
+    public GameObject DApple_white_Prefab;
+    float span = 1.0f;
+    float delta = 0;
+
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject go = Instantiate(DApple_white_Prefab) as GameObject;
+            int px = Random.Range(-10, 10);
+            go.transform.position = new Vector3(px, 8, 0); //가운데 숫자가 y좌표
+        }
+    }
+}
