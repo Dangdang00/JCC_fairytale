3dbe56521cdc742240542179f4b3249c18223d51
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-26 02:30:04 +0900

    만든 사람들 추가

---
+++ JCC_fairytale/Assets/Script/TitleScene_Change.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene_Change : MonoBehaviour
+{
+    public void ChangeTitleScene()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void ChangeStoryScene()
+    {
+        SceneManager.LoadScene("story");
+    }
+
+    public void ChangeRuleScene()
+    {
+        SceneManager.LoadScene("Rule");
+    }
+
+    public void ChangeChoiceScene()
+    {
+        SceneManager.LoadScene("Choice");
+    }
+
+    public void ChangeLibraryScene()
+    {
+        SceneManager.LoadScene("Library");
+    }
+
+    public void ChangePeopleScene()
+    {
+        SceneManager.LoadScene("people");
+    }
+}
