7035b53d822d26ecf6c381522709483f377174f6
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-28 00:36:39 +0900

    사운드 추가

---
+++ JCC_fairytale/Assets/Script/AudioManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject); // 이렇게 하면 다음 scene으로 넘어가도 음악 유
+    }
+}
--- JCC_fairytale/Assets/Script/R_Script/ChangeScene.cs
+++ JCC_fairytale/Assets/Script/R_Script/ChangeScene.cs
@@ -4,8 +4,16 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    AudioSource audioSource;    // 선언
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();  // 초기화
+    }
+
     public void ChangeGameScene()
     {
+        audioSource.Play(); // 실행
         SceneManager.LoadScene("R_Game");
     }
 }
