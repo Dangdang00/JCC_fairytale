bc9cbc14335e9700c777df8c06ac13020fa2a90c
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-05-27 18:27:11 +0900

    타이틀 화면 음악 추가

--- JCC_fairytale/Assets/Script/TitleScene_Change.cs
+++ JCC_fairytale/Assets/Script/TitleScene_Change.cs
@@ -5,23 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class TitleScene_Change : MonoBehaviour
 {
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        //DontDestroyOnLoad(gameObject);
+    }
+
     public void ChangeTitleScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Title");
     }
 
     public void ChangeStoryScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("story");
     }
 
     public void ChangeRuleScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Rule");
     }
 
     public void ChangeChoiceScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("Choice");
     }
 
@@ -32,6 +44,7 @@ public class TitleScene_Change : MonoBehaviour
 
     public void ChangePeopleScene()
     {
+        audioSource.Play();
         SceneManager.LoadScene("people");
     }
 }
