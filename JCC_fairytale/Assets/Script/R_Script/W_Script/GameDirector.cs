1ce26153261d661ab95a1780312fafa2796e006f
areumO2 <Areum Yang>
2019-05-30 22:13:36 +0900

    스코어 텍스트 추가

--- JCC_fairytale/Assets/Script/R_Script/W_Script/GameDirector.cs
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/GameDirector.cs
@@ -6,16 +6,29 @@ using UnityEngine.SceneManagement;
 
 public class GameDirector : MonoBehaviour {
     GameObject hpGage;
+    public Text scoreText;
+    int score = 0;
+    public static GameDirector instance;
+
+    public void AddScore(int enemyScore)
+    {
+        score += enemyScore;
+        scoreText.text = "Score:" + score; //스코어에 올립니다.
+    }
+
+
     void Start() {
         this.hpGage = GameObject.Find("HPImage");
         Invoke("WhiteGameWin", 30.0f);
     }
+
     public void DecreaseHP() {
         this.hpGage.GetComponent<Image>().fillAmount -= 0.2f;
         if (this.hpGage.GetComponent<Image>().fillAmount == 0f) {
             SceneManager.LoadScene("WhiteSnowLose");
         }
     }
+
     public void WhiteGameWin() {
         SceneManager.LoadScene("WhiteSnowWin");
     }
