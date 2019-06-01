7e91ca468bba78499eb6cefd3a29b79ec89080c7
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-01 23:23:41 +0900

    빨간모자 러닝게임 중력 변경

--- JCC_fairytale/Assets/Script/R_Script/Jump.cs
+++ JCC_fairytale/Assets/Script/R_Script/Jump.cs
@@ -11,21 +11,23 @@ public class Jump : MonoBehaviour
         /*GetMousButton(0)하면 누르면 계속 점프*/
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Click");
             if (isJumping == false)
             {
                 isJumping = true;
                 Debug.Log("true!");
-                GetComponent<Rigidbody2D>().AddForce(Vector3.up * 350f);
+                GetComponent<Rigidbody2D>().AddForce(Vector3.up * 400f);
             }
         }
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
-       // if (col.transform.name == "Ground")
-       // {
+       if (col.transform.tag == "Ground")
+       {
             isJumping = false;
             Debug.Log("False!");
-       // }
+       }
+
     }
 }
