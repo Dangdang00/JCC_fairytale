7671c96307989817563e7f75e60ad738c63ff42e
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-04 22:37:21 +0900

    각 동화 단계별 잠금 추가

--- JCC_fairytale/Assets/Script/R_Script/ChangeBookCover.cs
+++ JCC_fairytale/Assets/Script/R_Script/ChangeBookCover.cs
@@ -9,14 +9,33 @@ public class ChangeBookCover : MonoBehaviour
     public GameObject Rbook;
     public GameObject Wbook;
     public GameObject Hbook;
+
     public GameObject Rbook_Clear;
     public GameObject Wbook_Clear;
     public GameObject Hbook_Clear;
+
+    public GameObject Rbook_Lock;
+    //public GameObject Wbook_Lock;
+    public GameObject Hbook_Lock;
 
     public GameObject End_Panel;
 
     void Update()
     {
+        // 백설공주 게임을 성공하면 헨젤과 그레텔 Lock 해제
+        if(Go_ending.Wbook == true)
+        {
+            Hbook.SetActive(true);
+            Hbook_Lock.SetActive(false);
+        }
+
+        // 백설공주와 헨젤과 그레텔 게임을 모두 성공하면 Lock 해제
+        if (GoEnd.Hbook == true && Go_ending.Wbook == true)
+        {
+            Rbook.SetActive(true);
+            Rbook_Lock.SetActive(false);
+        }
+
         // 빨간모자 미니게임 성공시 책 이미지 변경
         if (Go_Ending.Rbook)
         {
@@ -40,9 +59,8 @@ public class ChangeBookCover : MonoBehaviour
 
         if (Go_Ending.Rbook && Go_ending.Wbook && GoEnd.Hbook)
         {
-            // 딜레이 주
+            // 딜레이 주기
             End_Panel.SetActive(true);
         }
-
     }
 }
