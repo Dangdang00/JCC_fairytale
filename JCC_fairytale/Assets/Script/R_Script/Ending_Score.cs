55024e288f02cb7cb658e16805c7675b49917ade
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-10 21:09:15 +0900

    빨간모자 스코어 수정

---
+++ JCC_fairytale/Assets/Script/R_Script/Ending_Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending_Score : MonoBehaviour
+{
+    int ResultScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
