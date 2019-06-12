4202e28cd7e7605a6b0ac8a77b15b3c37e156db3
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-12 23:24:41 +0900

    백설공주 스코어 변경

--- JCC_fairytale/Assets/Script/R_Script/W_Script/ResultScore.cs
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/ResultScore.cs
@@ -5,31 +5,19 @@ using UnityEngine.UI;
 
 public class ResultScore : MonoBehaviour
 {
-    public static Text ResultText;
-    public static int WResultScore = 0;
+    public Text ResultText;
+    int WResultScore = 0;
 
     void Start()
     {
         WResultScore = 0;
     }
 
-    public static void setWResultScore(int score)
-    {
-        Debug.Log("값 받음" + score);
-        WResultScore = score;
-        Debug.Log("값 대입" + WResultScore);
-        //ResultText.text = WResultScore.ToString();
-    }
-
-
-    /*void Update()
+    void Update()
     {
         Debug.Log(WResultScore);
-        printResultScore();
-    }
-
-    public void printResultScore()
-    {
+        WResultScore = Score.Wcount;
         ResultText.text = WResultScore.ToString();
-    }*/
+
+    }
 }
