4b3f48e6d88429ea0705af8dd0b0be98040814d9
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-13 13:13:22 +0900

    헨젤과 그레텔 미니게임 생명 추가

---
+++ JCC_fairytale/Assets/Script/H_Script/HUD.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HUD : MonoBehaviour
+{
+    public Sprite[] HeartSprites;
+    public Image HeartUI;
+    private PlayerCtrl player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<PlayerCtrl>();
+
+    }
+
+    private void Update()
+    {
+        HeartUI.sprite = HeartSprites[player.hp / 6];
+    }
+}
