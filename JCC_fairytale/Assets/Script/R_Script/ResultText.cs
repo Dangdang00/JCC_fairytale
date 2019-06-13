cb2ed616206386057d8d4d544757bdd5bf8f4727
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-13 11:32:56 +0900

    전체 게임 스코어 변경

--- JCC_fairytale/Assets/Script/R_Script/ResultText.cs
+++ JCC_fairytale/Assets/Script/R_Script/ResultText.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ResultText : MonoBehaviour
-{
-    public int result_Score;
+{   
+    int result_Score;
     public Text scoreText;
 
-   public static void Print_Score(int score)
+    void Update()
     {
-        Debug.Log("Call Function");
-        //result_Score = score;
-        //scoreText.text = result_Score.ToString();
+        result_Score = ScoreText.Rscore;
+        Debug.Log("RedHood: " + result_Score);
+        scoreText.text = result_Score.ToString();
     }
 }
---
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/EndScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour
+{
+    int Wscore = 0;
+    public Text WScoreText;
+
+    void Update()
+    {
+        Wscore = Score.Wcount;
+        Debug.Log("SnowWhite: " + Wscore);
+        WScoreText.text = Wscore.ToString();
+    }
+}
