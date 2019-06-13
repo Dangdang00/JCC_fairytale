3036b4cc7e7b847000fccc80f6ba54c0a3d8d5a1
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-13 20:12:11 +0900

    동화 별 스코어 스크립트  추가

---
+++ JCC_fairytale/Assets/Script/R_Script/REndScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class REndScore : MonoBehaviour
+{   
+    int result_Score;
+    public Text scoreText;
+
+    void Update()
+    {
+        result_Score = ScoreText.Rscore;
+        Debug.Log("RedHood: " + result_Score);
+        scoreText.text = result_Score.ToString();
+    }
+}
---
+++ JCC_fairytale/Assets/Script/R_Script/W_Script/Go_WDB.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Go_WDB : MonoBehaviour
+{
+    public void Go_WDB_Scene()
+    {
+        SceneManager.LoadScene("W_DB");
+    }
+}
