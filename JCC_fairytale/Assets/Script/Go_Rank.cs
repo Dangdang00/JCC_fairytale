7a0e73900a847a75a81e302c6907b5a9ca36c389
Dayoung Hwang <s2018s39@e-mirim.hs.kr>
2019-06-15 23:48:03 +0900

    전체 랭킹 화면 씬 추가

---
+++ JCC_fairytale/Assets/Script/Go_Rank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go_Rank : MonoBehaviour
+{
+    public void RRank()
+    {
+        SceneManager.LoadScene("R_Rank");
+    }
+
+    public void WRank()
+    {
+        SceneManager.LoadScene("W_Rank");
+    }
+
+    public void HRank()
+    {
+        SceneManager.LoadScene("H_Rank");
+    }
+}
